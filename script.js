9253b769ae2f7877601d7d3384db6094aacad604
Shivam Murawala <Shivammurawala2810@gmail.com>
2025-03-24 19:11:15 -0400

    Fixed bugs
    Essentially the fetch statements were synchronous and all of them ran regardless of errors in the first one. I move the last 2 fetches to a .then that only runs if the previous .then resolves it's promise. This allows data to remain there until the user input is successful. Also fixed the city not valid alert handeling. Next, I want to add more comments, give more console.logs for debugging purposes and add a dark mode.

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ const pexelsKey = "";
 
 function calculateWeather() {
   const city = document.getElementById('city-input').value;
+  let cityIssue = false;
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(res => {
-      if (!res.ok) throw new Error('City not found');
+      if (!res.ok) {
+        cityIssue = true;
+        throw new Error('City not found');
+      }
       return res.json();
     })
     .then(data => {
@@ -25,37 +29,48 @@ function calculateWeather() {
       descElement.textContent = `${data.name}: ${data.weather[0].description}`;
       iconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
       iconElement.style.display = "block";
+      return "continue";
     })
-    .catch(err => alert('Error fetching weather data'));
+    .then(result => {
+      // New forecast implementation
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(forecastData => {
+          const forecastBox = document.getElementById("hourly-forecast");
+          forecastBox.innerHTML = '';
 
-  // New forecast implementation
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-    .then(res => res.json())
-    .then(forecastData => {
-      const forecastBox = document.getElementById("hourly-forecast");
-      forecastBox.innerHTML = '';
+          for (let i = 0; i < 8; i++) {
+            const hourData = forecastData.list[i];
+            const hour = new Date(hourData.dt * 1000).getHours();
+            const temp = Math.round(hourData.main.temp - 273.15);
 
-      for (let i = 0; i < 8; i++) {
-        const hourData = forecastData.list[i];
-        const hour = new Date(hourData.dt * 1000).getHours();
-        const temp = Math.round(hourData.main.temp - 273.15);
-
-        forecastBox.innerHTML += `
-          <div class="hourly-container">
-            <span>${hour}:00</span>
-            <img src="https://openweathermap.org/img/wn/${hourData.weather[0].icon}.png">
-            <span>${temp}°C</span>
-          </div>`;
+            forecastBox.innerHTML += `
+              <div class="hourly-container">
+                <span>${hour}:00</span>
+                <img src="https://openweathermap.org/img/wn/${hourData.weather[0].icon}.png">
+                <span>${temp}°C</span>
+              </div>`;
+          }
+        });
+      fetch(`https://api.pexels.com/v1/search?query=${city}&per_page=1`, {
+        headers: { Authorization: pexelsKey }
+      })
+        .then(res => res.json())
+        .then(photoData => {
+          if (photoData.photos?.length > 0) {
+            document.body.style.backgroundImage = `url('${photoData.photos[0].src.large2x}')`;
+          }
+        })
+        .catch(() => console.log('Background fetch failed'));
+    })
+    .catch(err => {
+      if (cityIssue) {
+        alert("Please enter a valid city")
+      }
+      else {
+        alert('Error fetching weather data')
       }
     });
-  fetch(`https://api.pexels.com/v1/search?query=${city}&per_page=1`, {
-    headers: { Authorization: pexelsKey }
-  })
-    .then(res => res.json())
-    .then(photoData => {
-      if (photoData.photos?.length > 0) {
-        document.body.style.backgroundImage = `url('${photoData.photos[0].src.large2x}')`;
-      }
-    })
-    .catch(() => console.log('Background fetch failed'));
+
+
 }
